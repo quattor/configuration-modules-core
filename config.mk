62f1c9a8fe994ed8bd1a8ce1d0dba6135e5bb573
davidoc <davidoc@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2009-02-09 14:10:15 +0000

    2009-02-09  David O'Callaghan
    	* Release: 4.0.1
    	- Speed up checks of existing group membership
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/core/ncm-accounts@262 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 COMP=accounts
 NAME=ncm-$(COMP)
 DESCR=accounts NCM component.
-VERSION=4.0.0
+VERSION=4.0.1
 RELEASE=1
 AUTHOR=Charles Loomis <charles.loomis@cern.ch>, Stephen Childs <childss@cs.tcd.ie>
 MAINTAINER=Stephen Childs <childss@cs.tcd.ie>
@@ -14,4 +14,4 @@ TESTVARS=
 MANSECT=8
 MANDIR=
 
-DATE=12/12/08 18:27
+DATE=09/02/09 14:08
