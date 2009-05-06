8be897929884dbabf8ee701a013398df8c9942df
jouvin <jouvin@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2009-05-06 20:22:23 +0000

    2009-05-06  Michel Jouvin
    	* Release: 4.0.5
    	- Clean up and optimize home directory parent check/creation
    	Cleanup code formatting (in particular ident) for better readability
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/core/ncm-accounts@860 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 COMP=accounts
 NAME=ncm-$(COMP)
 DESCR=accounts NCM component.
-VERSION=4.0.4
+VERSION=4.0.5
 RELEASE=1
 AUTHOR=Charles Loomis <charles.loomis@cern.ch>, Stephen Childs <childss@cs.tcd.ie>
 MAINTAINER=Stephen Childs <childss@cs.tcd.ie>
@@ -14,4 +14,4 @@ TESTVARS=
 MANSECT=8
 MANDIR=
 
-DATE=06/05/09 17:02
+DATE=06/05/09 22:20
