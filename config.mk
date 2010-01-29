7c4ee85dbaf969d8984c51e38d6b7700f4e338d3
luisfmunoz <luisfmunoz@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2010-01-29 18:30:15 +0000

    2010-01-29  Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>
    	* Release: 5.0.0
    	- Full rewriting, to make it understandable and maintainable.
    	
    	In the road, bugs #61777, #55992, #50029 and #55596 have been fixed.
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/core/ncm-accounts@1559 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -1,17 +1,19 @@
 COMP=accounts
 NAME=ncm-$(COMP)
 DESCR=accounts NCM component.
-VERSION=4.0.14
+VERSION=5.0.0
 RELEASE=1
-AUTHOR=Charles Loomis <charles.loomis@cern.ch>, Stephen Childs <childss@cs.tcd.ie>
-MAINTAINER=Stephen Childs <childss@cs.tcd.ie>
+AUTHOR=Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>, Charles Loomis <charles.loomis@cern.ch>, Stephen Childs <childss@cs.tcd.ie>
+MAINTAINER=Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>
 
 CCONFIGDIR=$(NCM_CONF)/$(COMP)
 CONFIGFILE=$(CONFIGDIR)/config
+
+NCM_EXTRA_REQUIRES=libuser
 
 TESTVARS=
 
 MANSECT=8
 MANDIR=
 
-DATE=12/11/09 19:39
+DATE=29/01/10 19:24
