1828b1be40449d3397a2b016410a49793867820b
luisfmunoz <luisfmunoz@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2010-02-02 11:27:58 +0000

    2010-02-02  Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>
    	* Release: 1.5.8
    	- Fix the permissions of the files modified by the component.
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/core/ncm-useraccess@1574 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- ncm-useraccess/src/main/perl/useraccess.pm
+++ ncm-useraccess/src/main/perl/useraccess.pm
@@ -5,7 +5,7 @@
 # File: useraccess.pm
 # Implementation of ncm-useraccess
 # Author: Luis Fernando Muñoz Mejías <mejias@delta.ft.uam.es>
-# Version: 1.5.7 : 27/11/09 14:36
+# Version: 1.5.8 : 02/02/10 12:27
 # 
 #
 # Note: all methods in this component are called in a
@@ -320,16 +320,19 @@ sub files
     $path = "$home/" . K4LOGIN;
     $h{KRB4()} = CAF::FileWriter->new($path, log => $self,
 				      owner => $uid,
-				      group => $gid);
+				      group => $gid,
+				      mode => 0400);
 
     $path = "$home/" . K5LOGIN;
     $h{KRB5()} = CAF::FileWriter->new($path, log => $self,
 				      owner => $uid,
-				      group => $gid);
+				      group => $gid,
+				      mode => 0400);
     $path = "$home/" . SSH_AUTH;
     $h{SSH_KEYS()} = CAF::FileWriter->new($path, log => $self,
 					  owner => $uid,
-					  group => $gid);
+					  group => $gid,
+					  mode => 0400);
 
     foreach my $cred (@{$uconfig->{MANAGED_CREDENTIALS()}}) {
 	$h{MANAGED_CREDENTIALS()}->{$cred} = 1;
