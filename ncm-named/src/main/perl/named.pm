f45db451c18212278eefd017b55c677ce9484704
jouvin <jouvin@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2010-02-02 19:00:21 +0000

    Rewrite with CAF::Process and getTree() API
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/core/ncm-named@1580 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- ncm-named/src/main/perl/named.pm
+++ ncm-named/src/main/perl/named.pm
@@ -27,7 +27,7 @@ use EDG::WP4::CCM::Element;
 
 use LC::File qw(copy);
 use LC::Check;
-use LC::Process qw(run);
+use CAF::Process;
 
 local(*DTA);
 
@@ -42,47 +42,44 @@ sub Configure {
 ##########################################################################
   my ($self,$config)=@_;
 
+  # Get config into a perl Hash
+  my $named_config = $config->getElement($base)->getTree();
+  
   # Check if named server must be enabled
 
   my $server_enabled = 0;
-  if ( ($config->elementExists("$base/start")) && ($config->getElement("$base/start")->getValue() eq $true) ) {
+  if ( $named_config{start} ) {
     $server_enabled = 1;
   }
 
 
   # Update resolver configuration file with appropriate servers
-  if ($config->elementExists("$base/servers") || $server_enabled ) {
-    my $named_servers;
-    if ($config->elementExists("$base/servers")) {
-      $named_servers = $config->getElement("$base/servers");
-    }
+  if ( $named_config{servers} || $server_enabled ) {
     my $changes += LC::Check::file("/etc/resolv.conf",
-				source => "/etc/resolv.conf",
-				backup => '.old',
-				code   => sub {
-				  return() unless @_;
-				  my @oldcontents = split /\n/, $_[0];
-				  my @newcontents;
-				  for my $line (@oldcontents) {
-				    if ($line !~ /^\s*nameserver\s+/i) {
-				      push @newcontents, $line;
-				    }
-				  };
-				  if ( $server_enabled ) {
-				    push @newcontents, "nameserver 127.0.0.1\t\t# added by Quattor"
-				  }
-				  while ($named_servers->hasNextElement()) {
-				    my $named_server = $named_servers->getNextElement()->getValue();
-				    push @newcontents, "nameserver $named_server\t\t# added by Quattor"
-				  }
-				  return(join "\n",@newcontents);
-				}
-			       );
-    if ($config->elementExists("$base/search")) {
-      my $search = $config->getElement("$base/search");
+                                   source => "/etc/resolv.conf",
+                                   backup => '.old',
+                                   code   => sub {
+                                                  return() unless @_;
+                                                  my @oldcontents = split /\n/, $_[0];
+                                                  my @newcontents;
+                                                  for my $line (@oldcontents) {
+                                                    if ($line !~ /^\s*nameserver\s+/i) {
+                                                      push @newcontents, $line;
+                                                    }
+                                                  };
+                                                  if ( $server_enabled ) {
+                                                    push @newcontents, "nameserver 127.0.0.1\t\t# added by Quattor"
+                                                  }
+                                                  for my $named_server (@{$named_config{servers}] {
+                                                    push @newcontents, "nameserver $named_server\t\t# added by Quattor"
+                                                  }
+                                                  return(join "\n",@newcontents);
+                                                 }
+                                  );
+    if ( $named_config{search} ) {
       my @domains;
-      while ($search->hasNextElement()) {
-        push @domains, $search ->getNextElement()->getValue();
+      for my $domain (@{$named_config{search}}) {
+        push @domains, $domain;
       }
       if ( @domains ){
         $changes += NCM::Check::lines("/etc/resolv.conf",
@@ -93,11 +90,10 @@ sub Configure {
     }
     
     # options
-    if ($config->elementExists("$base/options")) {
-      my $search = $config->getElement("$base/options");
+    if ( $named_config{options} ) {
       my @options;
-      while ($search->hasNextElement()) {
-        push @options, $search ->getNextElement()->getValue();
+      for my $option (@{$named_config{options}}) {
+        push @options, $option;
       }
       if ( @options ){
         $changes += NCM::Check::lines("/etc/resolv.conf",
@@ -113,49 +109,87 @@ sub Configure {
     }
   }
 
-  # Ignore named startup configuration if startup script is not present
+  # Ignore named startup configuration if startup script is not present (service not configured)
 
-  if ( -e "/etc/rc.d/init.d/named" ) {
-    my $server_state = "stop";
-    my $reboot_state = "off";
-    if ($server_enabled) {
-      $server_state = "restart";     # Use restart rather than start to activate modifications
-      $reboot_state = "on";
-
-      # Copy the correct config file into /etc if specified, 
-      # else use current configuration file
-
-      if ($config->elementExists("$base/configfile")) {
-	my $config_src = $config->getElement("$base/configfile")->getValue();
-      
-	my $changes += LC::Check::file("/etc/named.conf",
-				       source      => $config_src,
-				       destination => "/etc/named.conf",
-				       owner       => 0,
-				       mode        => 0644
-				      );
-	unless (defined($changes)) {
-	  $self->error('error modifying /etc/named.conf');
-	return;
-	}
-      }
-    }
-
-    # Make sure, that named will be started/stopped on boot (acccording to 'start')
-    unless (LC::Process::run("/sbin/chkconfig named $reboot_state")) {
-      $self->error("command \"/sbin/chkconfig named $reboot_state\" failed");
-      return;
-    }
-  
-    # Re-restart the named
-    unless (LC::Process::run("/sbin/service named $server_state")) {
-      $self->error("command \"/sbin/service named $server_state\" failed");
-      return;
-    }
-
-  } else {
-    $self->verbose("named startup script doesn't exist : ignoring named configuration");
+  my $service = "named";
+  my $cmd = CAF::Process->new(["/sbin/chkconfig --list $service"], log => $self);
+  $cmd->output();      # Also execute the command
+  unless ( $? ) {
+    $self->debug(1,"Service $service doesn't exist on current host. Skipping $service configuration.");
+    return(1);
   }
+
+  # Copy the correct config file into /etc if specified, 
+  # else use current configuration file
+
+  my $server_changes;
+  if ( $named_config{configfile} ) {
+      $self->info("Checking $service configuration (/etc/named.conf)...");
+      $server_changes = LC::Check::file("/etc/named.conf",
+                                     source      => $named_config{configfile},
+                                     destination => "/etc/named.conf",
+                                     owner       => 0,
+                                     mode        => 0644
+                                   );
+      unless (defined($server_changes)) {
+        $self->error('error modifying /etc/named.conf');
+      return;
+  }
+  
+  # Enable named service
+  
+  my $reboot_state;  
+  if ( $server_enabled ) {
+    $self->info("Enabling service $named...");
+    $reboot_state = "on";
+  } else {
+    $self->info("Disabling service $named...");
+    $reboot_state = "off";
+  }
+  my $cmd = CAF::Process->new(["/sbin/chkconfig --level 345 $service $state"], log => $self);
+  $cmd->output();      # Also execute the command
+  if ( $? ) {
+    $self->error("Error defining service $service state for next reboot.");
+  }
+
+  # Start named if enabled and not yet started.
+  # Stop named if running but disabled.
+  # Restart after a configuration change if enabled and started.
+
+  $self->debug(1,"Checking if service $service is started...");
+  my $named_started = 1;
+  $cmd = CAF::Process->new(["/sbin/service $service status"], log => $self);
+  $cmd->output();      # Also execute the command
+  unless ( $? ) {
+    $self->debug(1,"Service $service not running.");
+    $named_started = 0;
+  }
+  
+  my $action;
+  if ( $server_enabled ) {
+    if ( ! $named_started ) {
+      $action = 'start';
+    } else if ( $changes) {
+      $action = 'restart';
+    }
+  } else {
+    if ( $named_started ) {
+      $action = 'stop';
+    };
+  }
+  
+  if ( $action ) {
+    $self->info("Doing a $action of service $service...");
+    $cmd = CAF::Process->new(["/sbin/service $service $action"], log => $self);
+    my $cmd_output = $cmd->output();      # Also execute the command
+    unless ( $? ) {
+      $self->debug(1,"Failed to update service $service state.\nError message: $cmd_output");
+      $named_started = 0;
+    }
+  } else {
+    $self->info("No need to start/stop/restart service $named");
+  }
+  
 
   return;
 }
