ebbad48283d093f3d90b4e6f04fbfd4a139a9cac
philippon <philippon@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2010-02-25 14:51:38 +0000

    First commit for ncm-ipmi
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/core/ncm-ipmi@1745 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

---
+++ ncm-ipmi/src/main/pan/components/ipmi/config-rpm.pan
@@ -0,0 +1,18 @@
+# ${license-info}
+# ${developer-info}
+# ${author-info}
+
+
+unique template components/ipmi/config-rpm;
+
+include { 'components/ipmi/schema' };
+
+# Package to install
+"/software/packages"=pkg_repl("ncm-ipmi","1.0.1-1","noarch");
+
+'/software/components/ipmi/version' = '1.0.1';
+
+"/software/components/ipmi/dependencies/post" ?= list("spma");
+"/software/components/ipmi/active" ?= true;
+"/software/components/ipmi/dispatch" ?= true;
+
---
+++ ncm-ipmi/src/main/perl/ipmi.pm
@@ -0,0 +1,67 @@
+# ${license-info}
+# ${developer-info}
+# ${author-info}
+
+
+package NCM::Component::ipmi;
+#
+# a few standard statements, mandatory for all components
+#
+
+use strict;
+use NCM::Component;
+use vars qw(@ISA $EC);
+@ISA = qw(NCM::Component);
+$EC=LC::Exception::Context->new->will_store_all;
+use NCM::Check;
+use File::Copy;
+
+use EDG::WP4::CCM::Element;
+
+use LC::Check;
+
+use Encode qw(encode_utf8);
+
+local(*DTA);
+
+
+use EDG::WP4::CCM::Element;
+
+##########################################################################
+sub Configure($$) {
+##########################################################################
+  my ($self,$config)=@_;
+  my $base     = "/software/components/ipmi/";
+  my $ipmi_exec = "/usr/bin/ipmitool";
+
+  my $ipmi_config = $config->getElement($base)->getTree();
+
+  my $users   = $ipmi_config->{users};
+  my $channel = $ipmi_config->{channel};
+  my $net_interface = $ipmi_config->{net_interface};
+
+  for my $user (@{$users}) {
+	$userid = $user->{userid};
+        $login  = $user->{login};
+	$passwd = $user->{password};
+	$priv   = $user->{priv};
+
+	system ($ipmi_exec." user set name ".$userid." ".$login);
+	system ($ipmi_exec." user set password ".$userid." ".$passwd);
+#	system ($ipmi_exec." user priv ".$userid." ".$priv);
+	
+  }
+
+
+  system ($ipmi_exec." mc reset cold");
+
+  return; # return code is not checked.
+}
+
+
+
+sub ConfigureNetwork {
+	return;
+}
+
+1; # Perl module requirement.
