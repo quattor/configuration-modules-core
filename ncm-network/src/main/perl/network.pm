1ae5138f9fde52b6e7643fe8fcc8611c2c5b53db
stdweird <stdweird@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2010-03-18 15:15:44 +0000

    SDW: untested CERN nic list patch config
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/core/ncm-network@1857 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- ncm-network/src/main/perl/network.pm
+++ ncm-network/src/main/perl/network.pm
@@ -2,7 +2,7 @@
 # This is 'network.pm', a ncm-network's file
 ################################################################################
 #
-# VERSION:    1.2.4, 14/07/09 12:24
+# VERSION:    1.2.5, 14/07/09 12:24
 # AUTHOR:     Stijn De Weirdt 
 # MAINTAINER: Stijn De Weirdt 
 # LICENSE:    http://cern.ch/eu-datagrid/license.html
