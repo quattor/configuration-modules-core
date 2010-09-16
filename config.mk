ee521f4b6262cd40c2b5e40c7518b7f70328a43e
luisfmunoz <luisfmunoz@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2010-09-16 16:25:00 +0000

    2010-09-16  Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>
    	* Release: 5.0.4
    	- Modify existing accounts by dropping and re-creating.
    
    	Should fix bug #63125.
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/core/ncm-accounts@2162 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 COMP=accounts
 NAME=ncm-$(COMP)
 DESCR=accounts NCM component.
-VERSION=5.0.3
+VERSION=5.0.4
 RELEASE=1
 AUTHOR=Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>, Charles Loomis <charles.loomis@cern.ch>, Stephen Childs <childss@cs.tcd.ie>
 MAINTAINER=Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>
@@ -16,4 +16,4 @@ TESTVARS=
 MANSECT=8
 MANDIR=
 
-DATE=21/06/10 17:29
+DATE=16/09/10 18:22
