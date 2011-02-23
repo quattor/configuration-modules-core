b2bb1b24636a580ec917031b84b57d3ac0c8e9de
luisfmunoz <luisfmunoz@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2011-02-23 17:14:05 +0000

    2011-02-23  Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>
    	* Release: 5.1.3
    	- Fix a crash on handling with pool accounts.
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/core/ncm-accounts@2510 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 COMP=accounts
 NAME=ncm-$(COMP)
 DESCR=accounts NCM component.
-VERSION=5.1.2
+VERSION=5.1.3
 RELEASE=1
 AUTHOR=Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>, Charles Loomis <charles.loomis@cern.ch>, Stephen Childs <childss@cs.tcd.ie>
 MAINTAINER=Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>
@@ -16,4 +16,4 @@ TESTVARS=
 MANSECT=8
 MANDIR=
 
-DATE=11/01/11 17:27
+DATE=23/02/11 18:13
