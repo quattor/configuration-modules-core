2ea325fb5f05c9106a318b3e21e25182ed38569d
luisfmunoz <luisfmunoz@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2011-03-02 01:13:37 +0000

    2011-03-02  Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>
    	* Release: 5.2.1
    	- Better report modified and deleted accounts.
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/ncm-components/core/ncm-accounts@2658 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 COMP=accounts
 NAME=ncm-$(COMP)
 DESCR=accounts NCM component.
-VERSION=5.2.0
+VERSION=5.2.1
 RELEASE=1
 AUTHOR=Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>, Charles Loomis <charles.loomis@cern.ch>, Stephen Childs <childss@cs.tcd.ie>
 MAINTAINER=Luis Fernando Muñoz Mejías <Luis.Fernando.Munoz.Mejias@cern.ch>
@@ -16,4 +16,4 @@ TESTVARS=
 MANSECT=8
 MANDIR=
 
-DATE=01/03/11 21:53
+DATE=02/03/11 02:13
