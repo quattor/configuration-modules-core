64b4d8ef5fba43335885fe074b252611b4638171
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2012-01-18 14:42:41 +0100

    Add a more powerful schema to the component, and start implementing it.
    We implement fully slapd.conf's man page, as shipped with SL6. The old
    code is left there for backwards-compatibility purposes. I hope it
    will get removed.

--- ncm-openldap/src/main/pan/components/openldap/schema.pan
+++ ncm-openldap/src/main/pan/components/openldap/schema.pan
@@ -15,9 +15,189 @@ declaration template components/openldap/schema;
 
 include { 'quattor/schema' };
 
+type long_pow2 = long with (SELF==1||SELF==2||SELF==4||SELF==8||
+    SELF==16||SELF==32||SELF==64||SELF==128||SELF==256||SELF==512||
+    error("Only powers of two are accepted"));
+
+# Possible acceptable values
+type ldap_hash = string with match(SELF, '\{(S?(SHA|MD5))\}') ||
+     error ("Only secure hashes are accepted here: {SSHA, SHA, SMD5, MD5}");
+
+type ldap_sizelimit = {
+     "soft" ? long
+     "hard" ? long
+} with exists(SELF["soft"]) || exists(SELF["hard"]) ||
+     error("Either 'soft' or 'hard' limits must be supplied");
+
+type ldap_buffer_size = {
+     "listener" ? type_absoluteURI
+     "read" ? long
+     "write" ? long
+} with exists(SELF["read"]) || exists(SELF["write"]) ||
+     error("Either 'read' or 'write' limits must be supplied");
+
+type ldap_access = {
+     "what" : string
+     "who" ? string
+     "access" ? string
+     "control" ? string
+};
+
+type auth_regexp = {
+     "match" : string
+     "replace" : string
+};
+
+type ldap_syntax = string{};
+
+type ldap_global = {
+     "access" : ldap_access[]  = list()
+     "allow" ? string[]
+     "argsfile" ? string
+     "attributeoptions" ? string[]
+# Indexed by attribute name. See RFC4512 for all details.
+     "attributetype" ? ldap_syntax{}
+     "authid-rewrite" ? string
+     "authz-policy" ? string
+     "authz-regexp" : auth_regexp[] = list()
+     "concurrency" ? long
+    "conn_max_pending_auth" ? long
+    "defaultsearchbase" ? string
+    "disallow" ? string[]
+    "ditcontentrule" ? ldap_syntax{}
+    "gentlehup" : boolean = false
+    "idletimeout" ? long
+    "include" ? string
+    "ldapsyntax" ? ldap_syntax{}
+    # This must be a power of 2
+    "listener-threads" ? long_pow2
+    "localSSF" : long = 71
+    "logfile" ? string
+    "loglevel" ? long
+    "moduleload" ? string
+    "modulepath" ? string
+    "objectclass" ? ldap_syntax{}
+    "password-hash" : ldap_hash = "{SSHA}"
+    "password-crypt-salt-format" ? string
+    "pidfile" ? string
+    "referral" ? type_URI
+    "require" ? string[]
+    "reverse-lookup" : boolean = false
+    "rootDSE" ? string
+    "sasl-auxprops" ? string
+    "sasl-host" ? type_fqdn
+    "sasl-ream" ? string
+    "sasl-secprops" ? string
+    "schemadn" ? string
+    "security" ? string
+    "serverID" ? long(0..4095)
+    "sizelimit" ? ldap_sizelimit
+    "sockbuf_max_incoming" ? long
+    "sockbuf_max_incoming_auth" ? long
+    "sortvals" ? string[]
+    "tcp-buffer" ? ldap_buffer_size
+    "threads" : long(2..) = 16
+    "timelimit" ? long
+    "tool-threads" : long = 1
+    "writetimeout" ? long
+};
+
+type tls_options = {
+     "CipherSuite" : string = "HIGH"
+     "CACertificateFile" ? string
+     "CACertificatePath" ? string
+     "CertificateKeyFile" ? string
+     "DHParamFile" ? string
+     "RandFile" ? string
+     "VerifyClient" ? string with match(SELF, "^(never|allow|try|hard|demand|true)$")
+     "CRLCheck" ? string with match(SELF, "^(none|peer|all)$")
+     "CRLFile" ? string
+};
+
+type ldap_database_string = string with
+     match(SELF, "^(bdb|config|dnssrv|hdb|ldap|ldif|meta|" +
+	   "monitor|null|passwd|perl|relay|shell|sql)$") ||
+     error("Unknown LDAP database type. " +
+	   "Check sladpd.conf man page");
+
+type ldap_ops = string with
+     match(SELF, "^(add|bind|compare|delete|modify|rename|search|read|write|"+
+	   '(extended=\w+)|rename)$');
+
+type ldap_replica_retries = {
+     "interval" : string
+     "retries" : long
+};
+
+type ldap_replica_cfg = {
+     "rid" : long(0..999)
+     "provider" : type_absoluteURI
+     "searchbase" : string
+     "type" ? string with match(SELF, "^(refreshOnly|refreshAndPersist)$")
+     "interval" ? string
+     "retry" ? ldap_replica_retries[]
+     "scope" ? string with match(SELF, "^(sub|one|base|subord)$")
+     "attrs" ? string[]
+     "attrsonly" ? boolean
+     "sizelimit" ? long
+     "timelimit" ? long
+     "schemachecking" : boolean = false
+     "network-timeout" ? long
+     "timeout" ? long
+     "bindmethod" ? string with match(SELF, "^(simple|sasl)$")
+     "binddn" ? string
+     "saslmech" ? string
+     "authcid" ? string
+     "authzid" ? string
+     "credentials" ? string
+     "realm" ? string
+     "secprops" ? string
+     "keepalive" ? string
+     "starttls" ? string with match(SELF, "^(yes|critical)$")
+     "tls_cert" ? string
+     "tls_key" ? string
+     "tls_cacert" ? string
+     "tls_cacertdir" ? string
+     "tls_reqcert" ? string with match(SELF, "^(never|allow|try|demand)$")
+     "tls_ciphersuite" ? string
+     "tls_crlcheck" ? string with match(SELF, "^(none|peer|all)$")
+     "suffixmassage" ? string
+     "logbase" ? string
+     "logfilter" ? string
+     "syncdata" ? string with match(SELF, "^(default|accesslog|changelog)$")
+};
+
+type ldap_database_limits = {
+     "size" ? ldap_sizelimit
+     "time" ? ldap_sizelimit
+};
+
+type ldap_database = {
+     "class" : ldap_database_string
+     "add_content_acl" : boolean = false
+     "extra_attrs" ? string[]
+     "hidden" : boolean = false
+     "lastmod" : boolean = true
+     "limits" ? ldap_database_limits{}
+     "maxderefdepth" : long = 15
+     "mirrormode" : boolean = false
+     "monitoring" ? boolean
+     "overlay" ? string
+     "readonly" ? boolean = false
+     "restrict" ? ldap_ops[]
+     "rootdn" ? string
+     "rootpw" ? string
+     "suffix" ? string
+     "subordinate" ? boolean
+     "sync_use_subentry" ? boolean
+     "syncrepl" ? ldap_replica_cfg
+     "updatedn" ? string
+     "updateref" ? type_absoluteURI
+};
+
 type component_openldap = {
 	include structure_component
-	'conf_file'		?	string
+	'conf_file'		: string = "/etc/openldap/slapd.conf"
 	'include_schema'	: string[]
 	'loglevel' 		? long(0..)
 	'pidfile' 		? string
@@ -28,6 +208,9 @@ type component_openldap = {
 	'rootpw'		: string
 	'directory'		: string
 	'index'			? string[]
+	'global_options' ? ldap_global
+	'backends'	? ldap_database[]
+	'databases'	? ldap_database[]
 };
 
 bind '/software/components/openldap' = component_openldap;
--- ncm-openldap/src/main/perl/openldap.pm
+++ ncm-openldap/src/main/perl/openldap.pm
@@ -18,124 +18,264 @@ use NCM::Component;
 use vars qw(@ISA $EC);
 @ISA = qw(NCM::Component);
 $EC=LC::Exception::Context->new->will_store_all;
-use NCM::Check;
-use LC::Process qw (output execute);
-use EDG::WP4::CCM::Element;
-
+use CAF::FileWriter;
+use CAF::Process;
 use File::Path;
 use File::Basename;
 
-local(*DTA);
+use Readonly;
 
-##########################################################################
-sub Configure($$@) {
-##########################################################################
+use constant PADDING => " "x4;
+use constant SLAPTEST => qw(/usr/sbin/slaptest -f /proc/self/fd/0 -v);
 
-    my $loglevel;
-    my $pidfile;
-    my $argsfile;
-    my $database;
-    my $suffix;
-    my $rootdn;
-    my $rootpw;
-    my $directory;
-    my $openldap_config_file;
+# Prints the replica information for a database into $fh
+sub print_replica_information
+{
+    my ($self, $fh, $tree) = @_;
 
-    my ($self, $config) = @_;
+    my @flds = qw(syncrepl);
 
-    # Define paths for convenience. 
-    my $base = "/software/components/openldap";
-
-    if ($config->elementExists("$base/conf_file")) {
-	$openldap_config_file = $config->getValue("$base/conf_file");
-    } else {
-	$openldap_config_file = '/tmp/test.conf';
+    $self->verbose("Printing database replica information");
+    if (exists($tree->{attrsonly})) {
+    	push(@flds, "attrsonly");
+    	delete($tree->{attrsonly});
     }
 
-    # write the slapd.conf
-    open(FILE, ">".$openldap_config_file);
+    if (exists($tree->{schemachecking})) {
+	push(@flds, "schemachecking=" . ($tree->{schemachecking}?"on":"off"));
+	delete($tree->{schemachecking});
+    }
 
-    my $ldap_config = $config->getElement($base)->getTree();
+    if (exists($tree->{retries})) {
+    	my $rt = sprintf(qq{retries="%s"},
+    			 join(" ", map("$_->{interval} $_->{retries}",
+    				       @{$tree->{retries}})));
+    	push(@flds, $rt);
+    	delete($tree->{retries});
+    }
 
-    my $include_entries = $ldap_config->{include_schema};
-    if($include_entries) {
-   	for my $entry (@{$include_entries}) {
-	    print FILE "include $entry\n";
+    while (my ($k, $v) = each(%$tree)) {
+    	push(@flds, "$k=$v");
+    }
+
+    print $fh PADDING, join(" ", @flds), "\n";
+}
+
+# Prints a database or a backend section to $fh, based on the contents
+# of the configuration $tree.
+sub print_database_class
+{
+    my ($self, $fh, $type, $tree) = @_;
+
+    print $fh "$type $tree->{class}\n";
+    delete($tree->{class});
+
+    foreach my $i (qw(add_content_acl hidden lastmod mirrormode
+		      monitoring readonly)) {
+	if (exists($tree->{$i})) { 
+	    print $fh PADDING, $i, ($tree->{$i} ? "on":"off"), "\n";
+	    delete($tree->{$i});
 	}
     }
 
-    print FILE "\n";
+    if (exists($tree->{restrict})) {
+	print $fh PADDING, join(" ", "restrict", @{$tree->{restrict}}), "\n";
+	delete($tree->{restrict});
+    }
+
+    if (exists($tree->{limits})) {
+	while (my ($k, $v) = each(%{$tree->{limits}})) {
+	    print $fh PADDING, "limits $k";
+	    print $fh " size.soft $v->{size}->{soft}"
+		if exists($v->{size}->{soft});
+	    print $fh " size.hard $v->{size}->{hard}"
+		if exists($v->{size}->{hard});
+	    print $fh " time.soft $v->{time}->{soft}"
+		if exists($v->{time}->{soft});
+	    print $fh " time.soft $v->{time}->{hard}"
+		if exists($v->{time}->{hard});
+	    print $fh "\n";
+	}
+	delete($tree->{limits});
+    }
+
+    while (my ($k, $v) = each(%$tree)) {
+	print $fh PADDING, "$k $v\n";
+    }
+}
+
+# Prints the global options for the slapd.conf file.
+sub print_global_options
+{
+    my ($self, $fh, $t) = @_;
+
+    foreach my $i (@{$t->{access}}) {
+	print $fh "access to $i->{what}";
+	print $fh " by $i->{who}"
+	    if exists($i->{who});
+	print $fh " $i->{what}"
+	    if exists($i->{what});
+	print $fh " $i->{control}"
+	    if exists($i->{control});
+	print $fh "\n";
+    }
+
+    delete($t->{access});
+
+    print $fh map("authz-regexp $_->{match} $_->{replace}\n",
+		  @{$t->{"authz-regexp"}});
+    delete($t->{"authz-regexp"});
+
+
+    foreach my $i (qw(gentlehup reverse-lookup)) {
+	print $fh "$i ", $t->{$i} ? "on":"off", "\n"
+	    if exists($t->{$i});
+	delete($t->{$i});
+    }
+
+    foreach my $i (qw(attributetype ditcontentrule ldapsyntax objectclass)) {
+	next unless exists($t->{$i});
+	print $fh "$i ";
+	while (my ($k, $v) = each(%{$t->{$i}})) {
+	    print $fh " $k $v";
+	}
+	print $fh "\n";
+	delete($t->{$i});
+    }
+
+    while (my ($k, $v) = each(%$t)) {
+	if (!ref($v)) {
+	    print $fh "$k $v";
+	} elsif (ref($v) eq 'ARRAY') {
+	    print $fh join(" ", $k, @$v);
+	}
+	print $fh "\n";
+    }
+}
+
+# Returns whether the contents of $fh are a valid configuration for
+# SLAPD, that would allow to reload the service.
+sub valid_config
+{
+    my ($self, $fh) = @_;
+
+    $self->verb("Validating the generated configuration");
+    my $cmd = CAF::Process->new($SLAPTEST,
+				log => $self,
+				stdin => "$fh",
+				stdout => \my $out,
+				stderr => \my $err)->execute();
+
+    if ($?) {
+	$fh->cancel();
+	$self->error("Invalid slapd configuration generated");
+	$self->info("Standard output: $out");
+	$self->info("Standard error: $err");
+    } else {
+	$self->verbose("Generated output: $out\n$err");
+    }
+
+    return !$?;
+}
+
+sub Configure
+{
+    my ($self, $config) = @_;
+
+    my $t = $config->getElement($self->prefix())->getTree();
+
+    my $fh = CAF::FileWriter->new($t->{conf_file},
+				  log => $self,
+				  backup => '.old');
+
+    # We may have to run things in the legacy mode. Hopefully this
+    # will get removed.
+    if ($t->{database} eq '') {
+	$self->print_global_options($fh, $t->{global_options});
+	foreach my $i (@{$t->{backends}}) {
+	    $self->print_database_class($fh, "backend", $i);
+	}
+	foreach my $i (@{$t->{databases}}) {
+	    $self->print_database_class($fh, "database", $i);
+	}
+    } else {
+	$self->legacy_setup($config, $fh);
+    }
+
+    if ($self->valid_config($fh)) {
+	$self->restart_slapd();
+	return 1;
+    }
+    return 0;
+}
+
+# Prints the slapd.conf file according to the previous, older, less
+# accurate schema.
+sub legacy_setup
+{
+    my ($self, $config, $fh) = @_;
+
+    foreach my $i (@{$t->{include_schema}}) {
+	print $fh "include $i\n";
+    }
+
+
 
     # get values if elements exist
     if ($config->elementExists("$base/database")) {
 	$database = $config->getValue("$base/database");
-	print FILE "database $database\n";
-    }
-    else {
-	die "database element not found : $!";
+	print $fh "database $database\n";
     }
 
-     if ($config->elementExists("$base/suffix")) {
+    if ($config->elementExists("$base/suffix")) {
 	$suffix = $config->getValue("$base/suffix");
-	print FILE "suffix $suffix\n";
-    }
-    else {
-	die "suffix element not found : $!";
+	print $fh "suffix $suffix\n";
     }
 
-     if ($config->elementExists("$base/rootdn")) {
+    if ($config->elementExists("$base/rootdn")) {
 	$rootdn = $config->getValue("$base/rootdn");
-	print FILE "rootdn $rootdn\n";
-    }
-    else {
-	die "rootdn element not found : $!";
+	print $fh "rootdn $rootdn\n";
     }
 
-     if ($config->elementExists("$base/rootpw")) {
+    if ($config->elementExists("$base/rootpw")) {
 	$rootpw = $config->getValue("$base/rootpw");
-	print FILE "rootpw $rootpw\n";
-    }
-    else {
-	die "rootpw element not found : $!";
+	print $fh "rootpw $rootpw\n";
     }
 
      if ($config->elementExists("$base/directory")) {
 	$directory = $config->getValue("$base/directory");
-	print FILE "directory $directory\n";
-    }
-    else {
-	die "directory element not found : $!";
+	print $fh "directory $directory\n";
     }
 
-    print FILE "\n";
+    print $fh "\n";
 
     if ($config->elementExists("$base/loglevel")) {
 	$loglevel = $config->getValue("$base/loglevel");
-	print FILE "loglevel $loglevel\n";
+	print $fh "loglevel $loglevel\n";
     }
 
     if ($config->elementExists("$base/pidfile")) {
 	$pidfile = $config->getValue("$base/pidfile");
-	print FILE "pidfile $pidfile\n";
+	print $fh "pidfile $pidfile\n";
     }
 
     if ($config->elementExists("$base/argsfile")) {
 	$argsfile = $config->getValue("$base/argsfile");
-	print FILE "argsfile $argsfile\n";
+	print $fh "argsfile $argsfile\n";
     }
 
-    print FILE "\n";
+    print $fh "\n";
 
     my $index_entries = $ldap_config->{index};
     if($index_entries) {
    	for my $entry (@{$index_entries}) {
-	    print FILE "index $entry\n";
+	    print $fh "index $entry\n";
 	}
     }
 
-    print FILE "\n";
+    print $fh "\n";
 
-    close(FILE);
+    $fh->close();
 
     return 1;
 }
