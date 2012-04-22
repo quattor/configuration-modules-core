60135aff296426863d72af470b700b95f92187aa
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2012-04-22 19:07:39 +0200

    Add the basic tests

---
+++ ncm-postfix/src/test/resources/NCM/Component.pm
@@ -0,0 +1,38 @@
+=pod
+
+=head1 DESCRIPTION
+
+Backup module, mimicking the base class for all NCM components, but
+with no real logic.
+
+=cut
+
+package NCM::Component;
+
+use strict;
+use warnings;
+use parent 'CAF::Object';
+use Template;
+
+sub _initialize
+{
+    my ($self, $name, $log) = @_;
+
+    $self->{name} = $name;
+    $self->{log} = $log || $main::this_app;
+
+    $self->{template} = Template->new(INCLUDE_PATH =>
+				      'target/share/templates');
+
+    return 1;
+}
+
+no strict 'refs';
+foreach my $i (qw(verbose error info ok debug)) {
+    *{$i} = sub {
+	my $self = shift;
+	return $self->{log}->$i(@_) if $self->{log};
+    }
+}
+
+1;
