cea9c2a57b6bb27b15d4bcea6200b54a764af3ee
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2013-04-04 22:23:30 +0200

    Fix typos in the documentation.

--- ncm-spma/src/main/perl/spma.pm
+++ ncm-spma/src/main/perl/spma.pm
@@ -138,8 +138,8 @@ C<$keeps_state> has a true value.
 
 If the command is executed, this method returns its standard output
 upon success or C<undef> in case of error.  If the command is not
-executed not executed the method always returns a true value (usually
-1, but don't rely on this!).
+executed the method always returns a true value (usually 1, but don't
+rely on this!).
 
 Yum-based commands require annoying error handling: they may exit 0
 even upon errors.  In those cases, we have to detect errors by looking
