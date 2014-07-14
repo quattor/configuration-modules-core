6508fab94d40985733a63260aa8f3c3fdfdfcb76
kwaegema <kenneth.waegeman@ugent.be>
2014-07-14 18:10:29 +0200

    lines too long

--- ncm-ceph/src/main/perl/Ceph/daemon.pm
+++ ncm-ceph/src/main/perl/Ceph/daemon.pm
@@ -35,7 +35,9 @@ our $EC=LC::Exception::Context->new->will_store_all;
 Readonly my $OSDBASE => qw(/var/lib/ceph/osd/);
 Readonly my $JOURNALBASE => qw(/var/lib/ceph/log/);
 
-Readonly::Array our @SSH_COMMAND => ('/usr/bin/ssh', '-o', 'ControlMaster=auto', '-o', 'ControlPersist=600', '-o', 'ControlPath=/tmp/ssh_mux_%h_%p_%r');
+Readonly::Array our @SSH_COMMAND => (
+'/usr/bin/ssh', '-o', 'ControlMaster=auto', '-o', 'ControlPersist=600', '-o', 'ControlPath=/tmp/ssh_mux_%h_%p_%r'
+);
 
 # get host of ip; save the map to avoid repetition
 sub get_host {
