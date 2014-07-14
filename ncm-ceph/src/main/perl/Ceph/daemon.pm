40812acde7caab1fe13374a9d964a562bef9b86c
kwaegema <kenneth.waegeman@ugent.be>
2014-07-14 14:56:06 +0200

    removal fix forgotten my

--- ncm-ceph/src/main/perl/Ceph/daemon.pm
+++ ncm-ceph/src/main/perl/Ceph/daemon.pm
@@ -49,7 +49,7 @@ sub get_host {
 # Gets the OSD map
 sub osd_hash {
     my ($self) = @_;
-    $jstr = $self->run_ceph_command([qw(osd dump)]) or return 0;
+    my $jstr = $self->run_ceph_command([qw(osd dump)]) or return 0;
     my $osddump = decode_json($jstr);  
     my %osdparsed = ();
     my $hostmap = {};
