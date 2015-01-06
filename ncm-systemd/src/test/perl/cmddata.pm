d6de32736e6645ed72ca5fed3d4fe0f3df05bd87
stdweird <stijn.deweirdt@ugent.be>
2015-01-06 09:59:31 +0100

    add initial unittests and test data

---
+++ ncm-systemd/src/test/perl/cmddata.pm
@@ -0,0 +1,41 @@
+# ${license-info}
+# ${developer-info}
+# ${author-info}
+# ${build-info}
+
+=pod
+
+=head1 cmddata module
+
+This module provides raw command data (output and exit code) and file content. 
+
+=cut
+package cmddata;
+
+use strict;
+use warnings;
+
+use File::Basename;
+my $dirname = dirname(__FILE__);
+
+# bunch of commands and their output
+our %cmds;
+our %files;
+
+# split in files due to too long
+sub _evalfn {
+    my $fn = shift;
+    open CODE, $fn;
+    undef $\;
+    my $code = join('', <CODE>);
+    close CODE;
+    eval $code;
+    die $@ if $@;
+}
+
+my @files = qw(service-systemctl_list service-systemctl_show service-legacy);
+foreach my $file (@files) {
+    _evalfn("$dirname/cmddata/$file");
+}
+
+1;
---
+++ ncm-systemd/src/test/resources/component.pan
@@ -0,0 +1,7 @@
+object template component;
+
+# Test the whole Config using this template
+
+"/software/components/systemd/default" = "ignore";
+
+include 'service-legacy_services';
