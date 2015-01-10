28e9d530bcb40cbd51e9d6005779b11599ecb428
stdweird <stijn.deweirdt@ugent.be>
2015-01-10 18:11:47 +0100

    add initial Unit module

---
+++ ncm-systemd/src/main/perl/Systemd/Service/Unit.pm
@@ -0,0 +1,116 @@
+# ${license-info}
+# ${developer-info}
+# ${author-info}
+# ${build-info}
+
+package NCM::Component::Systemd::Service::Unit;
+
+use 5.10.1;
+use strict;
+use warnings;
+
+use parent qw(CAF::Object Exporter);
+
+use Readonly;
+
+Readonly my $TARGET_DEFAULT => "default";
+Readonly my $TARGET_RESCUE => "rescue";
+Readonly my $TARGET_MULTIUSER => "multi-user";
+Readonly my $TARGET_GRAPHICAL => "graphical";
+Readonly my $TARGET_POWEROFF => "poweroff";
+Readonly my $TARGET_REBOOT => "reboot";
+Readonly my $DEFAULT_TARGET => $TARGET_MULTIUSER; # default level (if default.target is not responding)
+
+Readonly::Array my @TARGETS => qw($TARGET_DEFAULT $TARGET_RESCUE $TARGET_MULTIUSER $TARGET_GRAPHICAL
+                                $TARGET_POWEROFF $TARGET_REBOOT);
+
+our @EXPORT_OK = qw(@TARGETS $DEFAULT_TARGET);
+push(@EXPORT_OK, @TARGETS);
+
+our %EXPORT_TAGS = (
+    targets => [@TARGETS],
+);
+
+=pod
+
+=head1 NAME
+
+NCM::Component::Systemd::Service::Unit is a class handling services with units
+
+=head2 Public methods
+
+=over
+
+=item new
+
+Returns a new object, accepts the following options
+
+=over
+
+=item services
+
+A hash reference with service as key and a hash reference 
+with properties (according to the schema) as value.
+
+This is typical the return value of 
+     $config->getElement("/software/components/systemd/service")->getTree
+
+(and if needed, augmented with the conversion of legacy C<ncm-chkconfig> services via the 
+ NCM::Component::Systemd::Service::Chkconfig module).
+
+=item log
+
+A logger instance (compatible with C<CAF::Object>).
+
+=back
+
+=cut
+
+sub _initialize
+{
+    my $self  = {@_};
+
+    return 1;
+}
+
+
+=pod
+
+=item service_text
+
+Convert service C<detail> hash to human readable string.
+
+=cut
+
+sub service_text
+{
+    my ($self, $detail) = @_;
+    
+    my $text = "service $detail->{name} (";
+    $text .= "state $detail->{state} ";
+    $text .= "startstop $detail->{startstop} ";
+    $text .= "type $detail->{type} ";
+    $text .= "targets ".join(",", @{$detail->{targets}});
+    $text .= ")";
+    
+    return $text;
+}
+
+=pod
+
+=back
+
+=head2 Private methods
+
+=over
+
+=cut
+
+
+=pod
+
+=back
+
+=cut 
+
+1;
---
+++ ncm-systemd/src/test/perl/service-unit.t
@@ -0,0 +1,58 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::Quattor;
+
+use helper;
+use NCM::Component::systemd;
+
+$CAF::Object::NoAction = 1;
+
+# need a logger instance (could also use CAF::Object instance)
+my $cmp = NCM::Component::systemd->new('systemd');
+
+=pod
+
+=head1 DESCRIPTION
+
+Test C<NCM::Component::Systemd::Service::Unit> module for systemd.
+
+=head2 EXPORTS
+
+Test exports
+
+=cut
+
+use NCM::Component::Systemd::Service::Unit qw(:targets $DEFAULT_TARGET);
+is($DEFAULT_TARGET, $TARGET_MULTIUSER, "multiuser is default target");
+
+=head2 new
+
+Test creation of Unit instance
+
+=cut
+
+my $services = {};
+my $unit = NCM::Component::Systemd::Service::Unit->new(services => $services, log => $cmp);
+isa_ok($unit, "NCM::Component::Systemd::Service::Unit",
+        "NCM::Component::Systemd::Service::Unit instance created");
+
+=head2 service_text
+
+Test the generating text message from service details                                                                                                 
+                                                                                                                                                      
+=cut                                                                                                                                                  
+
+my $svc = {
+    name => "test_del",
+    state => "on",
+    type => "service",
+    startstop => 0,
+    targets => ["rescue"],
+};
+
+is($unit->service_text($svc), 
+   "service test_del (state on startstop 0 type service targets rescue)", 
+   "Generate string of service details");
+
+done_testing();
