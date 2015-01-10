8ed61348976d421a96ea736c51388d57b058fd09
stdweird <stijn.deweirdt@ugent.be>
2015-01-10 17:22:37 +0100

    Add systemctl interaction module

---
+++ ncm-systemd/src/main/perl/Systemd/Systemctl.pm
@@ -0,0 +1,113 @@
+# ${license-info}
+# ${developer-info}
+# ${author-info}
+# ${build-info}
+
+package NCM::Component::Systemd::Systemctl;
+
+use 5.10.1;
+use strict;
+use warnings;
+
+use Exporter 'import';
+our @EXPORT_OK = qw(systemctl_show);
+
+use Readonly;
+
+Readonly my $SYSTEMCTL => "/usr/bin/systemctl";
+
+
+=pod
+
+=head1 NAME
+
+NCM::Component::Systemd::Systemctl handle all systemd 
+interaction via C<systemctl> command.
+
+=head2 Public methods
+
+=over
+
+=item systemctl_show
+
+C<logger> is a mandatory logger to pass.
+
+Run C<systemctl show> on single C<$name> and return parsed output.
+If C<$name> is undef, the manager itself is shown.
+
+If succesful, returns a hashreference interpreting the C<key=value> output.
+Following keys have the value split on whitespace and a array reference 
+to the result as output
+
+=over
+=item Names
+=item Requires
+=item Wants
+=item WantedBy
+=item Before
+=item After
+=item Conflicts
+=back
+
+Returns undef on failure. 
+=cut
+
+sub systemctl_show
+{
+    my ($logger, $name) = @_;
+    my $proc = CAF::Process->new([$SYSTEMCTL, "--no-pager", "--all", "show"],
+                                  log => $logger,
+                                  );
+    if (defined($name)) {
+        $proc->pushargs($name);
+        $logger->verbose("service_systemctl_show for name $name");
+    } else {
+        $logger->verbose("service_systemctl_show for manager itself, name undefined");
+    }
+
+    my $output = $proc->output();
+    my $ec = $?;
+    if($ec) {
+        my $msg = "systemctl show failed (cmd $proc; ec $ec)";
+        $msg .= " with output $output" if (defined($output));
+        $logger->error($msg);
+        return;
+    }
+    
+    # output is k=[v]
+    # some keys will be split on whitespace
+    #  - when extending this list, update the pod!
+    my @isarray = qw(Names Requires Wants WantedBy Before After Conflicts);
+    my $res={};
+    while($output =~ m/^([^=\s]+)\s*=(.*)?$/mg) {
+        my ($k,$v) = ($1,"$2");
+        if (grep {$_ eq $k} @isarray) {
+            my @values = split(/\s+/, $v);
+            $res->{$k} = \@values;
+        } else {
+            $res->{$k} = $v;
+        }
+    }
+    
+    return $res;    
+}
+
+
+=pod
+
+=back
+
+=head2 Private methods
+
+=over
+
+=cut
+
+
+=pod
+
+=back
+
+=cut 
+
+1;
---
+++ ncm-systemd/src/test/perl/systemctl.t
@@ -0,0 +1,49 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::Quattor;
+use NCM::Component::systemd;
+use NCM::Component::Systemd::Systemctl qw(systemctl_show);
+
+use helper;
+
+$CAF::Object::NoAction = 1;
+
+=pod
+
+=head1 DESCRIPTION
+
+Test the C<Systemctl> module for systemd.
+
+=head2 systemctl_show
+
+Test systemctl_show
+
+=cut
+
+
+my ($res, @names);
+
+set_output("systemctl_show_runlevel6_target_el7");
+
+# need a logger instance (could also use CAF::Object instance)
+my $cmp = NCM::Component::systemd->new('systemd');
+$res = systemctl_show($cmp, 'runlevel6.target');
+
+is(scalar keys %$res, 63, "Found 63 keys");
+is($res->{Id}, 'reboot.target', "Runlevel6 is reboot.target");
+
+# test the split in array ref
+my @isarray = qw(Names Requires Wants WantedBy Before After Conflicts);
+foreach my $k (keys %$res) {
+    my $r = ref($res->{$k});
+    if (grep {$_ eq $k} @isarray) {
+        is($r, 'ARRAY', "$k is converted to array reference");
+    } else {
+        ok(! $r,  "$k is not a reference");
+    }
+}
+is_deeply($res->{Names}, ["runlevel6.target", "reboot.target"], "Runlevel6 names/aliases");
+
+
+done_testing();
