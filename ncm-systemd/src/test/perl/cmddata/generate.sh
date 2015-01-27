a18e07d68e0fa7ac89e894614b6bf7cbdf5c540f
stdweird <stijn.deweirdt@ugent.be>
2015-01-27 11:21:08 +0100

    update the test data and generate script

--- ncm-systemd/src/test/perl/cmddata/generate.sh
+++ ncm-systemd/src/test/perl/cmddata/generate.sh
@@ -5,38 +5,63 @@ reason=gen_${1:-X}
 
 fn=service_systemctl_list_show_$reason
 fnload=${fn}_load
-all=""
 rm -f $fn $fnload.pm
-for type in target service; do 
-  cmd="/usr/bin/systemctl list-unit-files --all --no-pager --no-legend --type $type"
-  name="${reason}_systemctl_list_unit_files_$type"
-  all="$all $name"
-  echo "\$cmds{$name}{cmd} = \"$cmd\";">>$fn
-  echo "\$cmds{$name}{out} = <<'EOF';">>$fn
-  $cmd >> $fn
-  echo EOF >> $fn 
-  echo >> $fn
-  for unit in `$cmd |sed -e "s/\@\?\.[^.]*\s\+.*$//;"`; do
-    name=`echo "${reason}_systemctl_show_${unit}_${type}_el7" | tr '-' '_' | tr '.' '_'`
+
+function begin_load () {
+    # load it in the cmddata module
+    # use this module in a .t file
+
+    echo "package cmddata::$fnload;" >> $fnload.pm
+    echo "use helper;" >> $fnload.pm
+    echo >> $fnload.pm
+}
+
+function end_load () {
+    echo "1;" >> $fnload.pm
+    echo "Created $fn $fnload.pm"
+}
+
+function add () {
+    name="$1"
+    cmd="$2"
+    ec="$3"
+    out="$4"
+
     all="$all $name"
-    cmd="/usr/bin/systemctl --no-pager --all show $unit.$type"
-    echo "\$cmds{$name}{cmd} = \"$cmd\";">>$fn
-    echo "\$cmds{$name}{out} = <<'EOF';">>$fn
-    $cmd >> $fn
+    echo "\$cmds{'$name'}{cmd} = '$cmd';">>$fn
+    echo "\$cmds{'$name'}{ec} = $ec;">>$fn
+    echo "\$cmds{'$name'}{out} = <<'EOF';">>$fn
+    echo "$out" >> $fn
     echo EOF >> $fn 
     echo >> $fn
-  done
+
+    # add to the load file
+    echo "set_output('$name');" >> $fnload.pm
+
+}
+
+begin_load
+
+
+for type in target service; do 
+    for list in unit-files units; do
+        cmd="/usr/bin/systemctl --all --no-pager --no-legend --full list-$list --type $type"
+        name="${reason}_systemctl_list-${list}_${type}"
+
+        out=`$cmd`
+        add "$name" "$cmd" $? "$out"
+
+        for unit in `echo "$out" |sed -e "s/\.$type\s\+.*$//;"`; do
+            name="${reason}_systemctl_show_${unit}_${type}_${list}"
+            # These might not be unique wrt list-unit and list-unit-files
+            # but should show same info
+            cmd="/usr/bin/systemctl --no-pager --all show $unit.$type"
+            echo "unit $unit name $name command $cmd"
+
+            out=`$cmd`
+            add "$name" "$cmd" $? "$out"
+        done            
+    done
 done
 
-# load it in the cmddata module
-# use this module in a .t file
-
-echo "package cmddata::$fnload;" >> $fnload.pm
-echo "use helper;" >> $fnload.pm
-echo >> $fnload.pm
-for i in $all; do
-  echo "set_output('$i');" >> $fnload.pm
-done
-echo "1;" >> $fnload.pm
-
-echo "Created $fn $fnload.pm"
+end_load
