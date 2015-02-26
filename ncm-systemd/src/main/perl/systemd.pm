b5a82cc28aef983d81070b30ee0ac86659f666b8
stdweird <stijn.deweirdt@ugent.be>
2015-02-26 17:50:52 +0100

    Enable processing of Systemd Service (Unit and/or Chkconfig based)

--- ncm-systemd/src/main/perl/systemd.pm
+++ ncm-systemd/src/main/perl/systemd.pm
@@ -8,11 +8,18 @@ use strict;
 use warnings;
 use base qw(NCM::Component);
 
+use NCM::Component::Systemd::Service;
+
 our $EC=LC::Exception::Context->new->will_store_all;
 $NCM::Component::${project.artifactId}::NoActionSupported = 1;
 
 sub Configure 
 {
+
+    my ($self, $config) = @_;
+
+    my $service = NCM::Component::Systemd::Service->new(log => $self);
+    $service->configure($config);
 
     return 1;
 }
