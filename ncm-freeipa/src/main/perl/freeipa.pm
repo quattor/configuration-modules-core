62cbd25eba3851faadcafeaed3822dcbb9443645
stdweird <stijn.deweirdt@ugent.be>
2016-08-02 11:01:05 +0200

    ncm-freeipa: add basic usage instructions

--- ncm-freeipa/src/main/perl/freeipa.pm
+++ ncm-freeipa/src/main/perl/freeipa.pm
@@ -1,6 +1,97 @@
 #${PMpre} NCM::Component::${project.artifactId}${PMpost}
 use base qw(NCM::Component CAF::Path);
 
+=head1 DESCRIPTION
+
+ncm-freeipa provides support for FreeIPA configuration for
+
+=over
+
+=item server: add users, groups, services
+
+=item client: retrieve keytabs and certificates
+
+=item initialisation: get started n an already deployed host
+
+=item AII: add initialisation in kickstart and support removal
+
+=back
+
+=head2 Server
+
+On the server, create a keytab for the quattor-server user
+    kinit admin
+
+    uidadmin=`ipa user-show admin |grep UID: |sed "s/UID://;s/ //g;"`
+    gidadmin=`ipa user-show admin |grep GID: |sed "s/GID://;s/ //g;"`
+    # keep random password; it's already expired
+    ipa user-add quattor-server --first=server --last=quattor --random --uid=$(($uidadmin+1)) --gidnumber=$(($gidadmin+1))
+    kdestroy
+    # use expired random password; and pick new random password (new password is not relevant)
+    kinit quattor-server
+    kdestroy
+
+    kinit admin
+    ipa role-add "Quattor server"
+    for priv in "Host Administrators" "DNS Administrators" "Group Administrators" "Service Administrators" "User Administrators"; do
+        ipa role-add-privilege "Quattor server" --privileges="$priv"
+    done
+    ipa role-add-member --users=quattor-server "Quattor server"
+
+
+    ipa-getkeytab -p quattor-server -k /etc/quattor-server.keytab -s ipaserver.example.com
+
+Use these with ncm-freeipa on the server.
+
+    prefix "/software/components/freeipa/principals/server";
+    "principal" = "quattor-server";
+    "keytab" = "/etc/quattor-server.keytab";
+
+(Do not retrieve a keytab for the admin user;
+it resets the admin password).
+
+=head2 AII
+
+First we need to add a user with appropriate priviliges
+    kinit admin
+
+    uidadmin=`ipa user-show admin |grep UID: |sed "s/UID://;s/ //g;"`
+    gidadmin=`ipa user-show admin |grep GID: |sed "s/GID://;s/ //g;"`
+    # keep random password; it's already expired
+    ipa user-add quattor-aii --first=aii --last=quattor --random --uid=$(($uidadmin+2)) --gidnumber=$(($gidadmin+2))
+    kdestroy
+    # use expired random password; and pick new random password (new password is not relevant)
+    kinit quattor-aii
+    kdestroy
+
+    ipa role-add "Quattor AII"
+    ipa role-add-privilege "Quattor AII" --privileges="Host Administrators"
+    ipa role-add-member --users=quattor-aii "Quattor AII"
+
+On the AII host do (assuming the host is already added to IPA)
+    kinit admin
+    ipa-getkeytab -p quattor-aii -k /etc/quattor-aii.keytab -s ipaserver.example.com
+    kdestroy
+
+And in the templates use
+    prefix "/software/components/freeipa/principals/aii";
+    "principal" = "quattor-aii";
+    "keytab" = "/etc/quattor-aii.keytab";
+
+=head2 Missing
+
+=over
+
+=item role / priviliges
+
+=item retrieve use keytabs
+
+=back
+
+=head1 Methods
+
+=cut
+
 use Readonly;
 use NCM::Component::FreeIPA::Client;
 use NCM::Component::FreeIPA::NSS;
