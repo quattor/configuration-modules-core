40f27f9620d5550dbcfbd7810284afd2306923e8
Kenneth Waegeman <kenneth.waegeman@ugent.be>
2018-01-17 12:00:31 +0100

    ncm-ceph: add Component Redirect and tqu test

--- ncm-ceph/src/main/perl/ceph.pm
+++ ncm-ceph/src/main/perl/ceph.pm
@@ -1,45 +1,12 @@
 #${PMpre} NCM::Component::ceph${PMpost}
 #
-our $EC=LC::Exception::Context->new->will_store_all;
 use parent qw(NCM::Component);
 our $NoActionSupported = 1;
 
-sub Configure
-{
-    my ($self, $config) = @_;
-    return $self->call_entry_point("Configure", $config);
-}
-
-sub Unconfigure
-{
-    my ($self, $config) = @_;
-    return $self->call_entry_point("Unconfigure", $config);
-}
-
-sub call_entry_point
-{
-    my ($self, $entry_point, $config) = @_;
-    my $t = $config->getElement($self->prefix())->getTree();
-
-    my $release;
-    if (defined($t->{release})) {
-        $release = $t->{release};
-    } else {
-        $release = 'Luminous';
-    }
-
-    my $submod = "NCM::Component::Ceph::$release";
-    eval "use $submod";
-    if ($@) {
-        $self->error("Failed to load $submod: $@");
-        return undef;
-    }
-
-    # No idea what this noaction stuff does, see spma.pm
-    my $NoAction = $self->{NoAction};
-    bless($self, "$submod");
-    $self->{NoAction} = $NoAction;
-    return $self->$entry_point($config);
-}
+use Readonly;
+Readonly our $REDIRECT => {
+    name => 'release',
+    default => 'Luminous',
+};
 
 1; # required for Perl modules
