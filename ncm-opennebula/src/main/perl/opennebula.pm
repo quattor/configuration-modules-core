1098f870c972134caba48bb5ca62d8743ba70823
Álvaro Simón <varosim@gmail.com>
2022-01-27 16:24:43 +0100

    ncm-opennebula: replace vm delete by vm terminate
    VM delete rpc call is not longer supported in opennebula 6.x.
    this fix requires latest Net::OpenNebula 0.317.0
    
    Resolves: #1538

--- ncm-opennebula/src/main/perl/opennebula.pm
+++ ncm-opennebula/src/main/perl/opennebula.pm
@@ -126,7 +126,7 @@ use CAF::FileReader;
 use CAF::Service;
 use Set::Scalar;
 use Config::Tiny;
-use Net::OpenNebula 0.316.0;
+use Net::OpenNebula 0.317.0;
 use Data::Dumper;
 use Readonly;
 use 5.10.1;
