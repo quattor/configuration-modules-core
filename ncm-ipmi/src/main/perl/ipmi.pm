12b65fb5b5438d8d69d580ea8c7a4f07d8153f03
Rob Johnson <quantumparticleresearch@gmail.com>
2022-04-13 15:19:18 +0100

    ncm-ipmi: Add more user account options to set privilege level and remote access. Enable IPMI over LAN by default

--- ncm-ipmi/src/main/perl/ipmi.pm
+++ ncm-ipmi/src/main/perl/ipmi.pm
@@ -21,6 +21,10 @@ sub Configure
 
   CAF::Process->new([qw(chkconfig ipmi on)], log => $self)->run();
   CAF::Process->new([qw(service ipmi restart)], log => $self)->run();
+  #Ensure IPMI over LAN is enabled
+  CAF::Process->new([IPMI_EXEC, qw(lan set), $channel, qw(access on)], log => $self)->run();
+  #Set user authentication to use MD5 security (about as good as it gets)
+  CAF::Process->new([IPMI_EXEC, qw(lan set), $channel, qw(auth USER MD5)], log => $self)->run();
 
   for my $user (@{$users}) {
       my $userid = $user->{userid};
@@ -28,9 +32,17 @@ sub Configure
       my $passwd = $user->{password};
       my $priv   = $user->{priv};
 
+      #Create and enable user account
       CAF::Process->new([IPMI_EXEC, qw(user set name), $userid, $login],
                         log => $self)->run();
       CAF::Process->new([IPMI_EXEC, qw(user set password), $userid, $passwd],
+                        log => $self)->run();
+      CAF::Process->new([IPMI_EXEC, qw(user set priv), $userid, sprintf("0x%X", $priv)],
+                        log => $self)->run();
+      CAF::Process->new([IPMI_EXEC, qw(user enable), $userid],
+                        log => $self)->run();
+      #Set remote access via LAN
+      CAF::Process->new([IPMI_EXEC, qw(channel setaccess), $channel, $userid, qw(link=on ipmi=on callin=on)],
                         log => $self)->run();
   }
 
